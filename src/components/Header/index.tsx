3302485acd9e5a70d3051239b0a1cf0a3a54f59a
Erick Macedo Pinto <erimacedo_92@hotmail.com>
2021-04-21 19:36:46 -0300

    Adds the current date to the Header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,20 @@
+import format from "date-fns/format";
+import enUS from "date-fns/locale/en-US";
+
 import styles from "./styles.module.scss";
 
 export function Header() {
+    const currentDate = format(new Date(), "EEEEEE, MMMM do", {
+        locale: enUS
+    });
+
     return (
         <header className={styles.headerContainer}>
             <img src="/logo.svg" alt="Podcastr" title="Podcastr logo" />
 
             <p>The best for you to listen, always</p>
 
-            <span>Th., April 8th</span>
+            <span>{currentDate}</span>
         </header>
     );
 }
