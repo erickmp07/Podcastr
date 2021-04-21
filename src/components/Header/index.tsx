3a04c34c14591dc2f6c849fb18412a63a11e4bd0
Erick Macedo Pinto <erimacedo_92@hotmail.com>
2021-04-21 19:17:13 -0300

    Sets the Header component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,11 @@
+export function Header() {
+    return (
+        <header>
+            <img src="/logo.svg" alt="Podcastr" title="Podcastr logo" />
+
+            <p>The best for you to listen, always</p>
+
+            <span>Th., April 8th</span>
+        </header>
+    );
+}
