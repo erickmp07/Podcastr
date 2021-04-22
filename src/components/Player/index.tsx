ecaf9d96091bfdeae1275004d85a2c189766627e
Erick Macedo Pinto <erimacedo_92@hotmail.com>
2021-04-21 22:06:43 -0300

    Adds elements to the Player component

--- src/components/Player/index.tsx
+++ src/components/Player/index.tsx
@@ -3,7 +3,16 @@ import styles from "./styles.module.scss";
 export function Player() {
     return (
         <div className={styles.playerContainer}>
+            <header>
+                <img src="/playing.svg" alt="Now playing" />
+                <strong>Now playing</strong>
+            </header>
 
+            <div className={styles.emptyPlayer}>
+                <strong>Choose a podcast to listen</strong>
+            </div>
+
+            <footer></footer>            
         </div>
     );
 }
