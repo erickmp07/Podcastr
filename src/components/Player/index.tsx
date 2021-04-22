853afe32890dcbcf9b9a160f0ceb52d34c103d8d
Erick Macedo Pinto <erimacedo_92@hotmail.com>
2021-04-21 21:55:04 -0300

    Adds style to the Player component

--- src/components/Player/index.tsx
+++ src/components/Player/index.tsx
@@ -2,6 +2,8 @@ import styles from "./styles.module.scss";
 
 export function Player() {
     return (
-        <div></div>
+        <div className={styles.playerContainer}>
+
+        </div>
     );
 }
