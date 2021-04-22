cac40bfb7eb95733aafaf38324c585b5c8bc12df
Erick Macedo Pinto <erimacedo_92@hotmail.com>
2021-04-21 22:22:35 -0300

    Adds Player footer elements

--- src/components/Player/index.tsx
+++ src/components/Player/index.tsx
@@ -4,7 +4,7 @@ export function Player() {
     return (
         <div className={styles.playerContainer}>
             <header>
-                <img src="/playing.svg" alt="Now playing" />
+                <img src="/playing.svg" alt="Now playing" title="Now playing" />
                 <strong>Now playing</strong>
             </header>
 
@@ -12,7 +12,36 @@ export function Player() {
                 <strong>Choose a podcast to listen</strong>
             </div>
 
-            <footer></footer>            
+            <footer className={styles.empty}>
+                <div className={styles.progress}>
+                    <span>00:00</span>
+                    <div className={styles.slider}>
+                        <div className={styles.emptySlider} />
+                    </div>
+                    <span>00:00</span>
+                </div>
+
+                <div className={styles.buttons}>
+                    <button type="button">
+                        <img src="/shuffle.svg" alt="Shuffle" title="Shuffle" />
+                    </button>
+                    <button type="button">
+                        <img src="/play-previous.svg" alt="Previous" title="Previous" />
+                    </button>
+                    <button type="button">
+                        <img src="/play.svg" alt="Play" title="Play" />
+                    </button>
+                    <button type="button" className={styles.playButton}>
+                        <img src="/play-previous.svg" alt="Previous" title="Previous" />
+                    </button>
+                    <button type="button">
+                        <img src="/play-next.svg" alt="Next" title="Next" />
+                    </button>
+                    <button type="button">
+                        <img src="/repeat.svg" alt="Repeat" title="Repeat" />
+                    </button>
+                </div>
+            </footer>            
         </div>
     );
 }
