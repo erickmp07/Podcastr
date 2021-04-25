e34f538c8fb3f12e4c193039708d40df43552aac
Erick Macedo Pinto <erimacedo_92@hotmail.com>
2021-04-24 23:20:15 -0300

    Implements getStaticProps() method in episode page

--- src/pages/episodes/[slug].tsx
+++ src/pages/episodes/[slug].tsx
@@ -1,4 +1,9 @@
+import { api } from "../../services/api";
+import { convertDurationToTimeString } from "../../utils/convertDurationToTimeString";
+import { GetStaticProps } from "next";
 import { useRouter } from "next/router";
+import { format, parseISO } from "date-fns";
+import enUS from "date-fns/locale/en-US";
 
 export default function Episode() {
     const router = useRouter();
@@ -6,4 +11,30 @@ export default function Episode() {
     return (
         <h1>{router.query.slug}</h1>
     );
+}
+
+export const getStaticProps: GetStaticProps = async (context) => {
+    const { slug } = context.params;
+    
+    const { data } = await api.get(`/episodes/${slug}`);
+
+    const episode = {
+        id: data.id,
+        title: data.title,
+        thumbnail: data.thumbnail,
+        members: data.members,
+        publishedAt: format(
+            parseISO(data.published_at), "d MMM yy", { locale: enUS }),
+        duration: Number(data.file.duration),
+        durationAsString: convertDurationToTimeString(Number(data.file.duration)),
+        description: data.description,
+        url: data.file.url
+    }
+
+    return {
+        props: {
+            episode
+        },
+        revalidate: 60 * 60 * 24
+    };
 }
