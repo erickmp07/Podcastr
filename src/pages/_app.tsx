e7c7d14649ce4f75c58d78f187061bbd73ce6785
Erick Macedo Pinto <erimacedo_92@hotmail.com>
2021-04-26 20:33:04 -0300

    Sets isPlaying state

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,11 +8,12 @@ import { useState } from "react";
 function MyApp({ Component, pageProps }) {
     const [episodes, setEpisodes] = useState([]);
     const [currentEpisodeIndex, setCurrentEpisodeIndex] = useState(0);
-    const [isPlaying, setIsPlaying] = useState(true);
+    const [isPlaying, setIsPlaying] = useState(false);
 
     function play(episode) {
         setEpisodes([episode]);
         setCurrentEpisodeIndex(0);
+        setIsPlaying(true);
     }
 
     return (
