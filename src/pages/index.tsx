c95d3b75678e1d7f85177e1fb14cde923a5fca8e
Erick Macedo Pinto <erimacedo_92@hotmail.com>
2021-04-26 19:06:41 -0300

    Invokes the play() to updates the Player Context

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,13 @@
 import { api } from "../services/api";
 import styles from "./home.module.scss";
+import { convertDurationToTimeString } from "../utils/convertDurationToTimeString";
+import { PlayerContext } from "../contexts/PlayerContext";
+import { useContext } from "react";
 import { GetStaticProps } from "next";
 import Image from "next/image";
 import Link from "next/link";
 import { format, parseISO } from "date-fns";
 import enUS from "date-fns/locale/en-US";
-import { convertDurationToTimeString } from "../utils/convertDurationToTimeString";
 
 type Episode = {
     id: string;
@@ -24,6 +26,8 @@ type HomeProps = {
 };
 
 export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
+    const { play } = useContext(PlayerContext);
+    
     return (
         <div className={styles.homepage}>
             <section className={styles.latestEpisodes}>
@@ -51,7 +55,7 @@ export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
                                     <span>{episode.durationAsString}</span>
                                 </div>
 
-                                <button type="button">
+                                <button type="button" onClick={() => play(episode)}>
                                     <img src="/play-green.svg" alt="Play episode" title="Play episode" />
                                 </button>
                             </li>
