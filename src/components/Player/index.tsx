e47ac92b7070f4b8894098fdee2dbd4911edf685
Erick Macedo Pinto <erimacedo_92@hotmail.com>
2021-04-26 21:04:16 -0300

    Changes isPlaying value by the audio tag events

--- src/components/Player/index.tsx
+++ src/components/Player/index.tsx
@@ -12,7 +12,8 @@ export function Player() {
         episodes, 
         currentEpisodeIndex, 
         isPlaying, 
-        togglePlay 
+        togglePlay,
+        setPlayingState 
     } = useContext(PlayerContext);
 
     useEffect(() => {
@@ -80,6 +81,8 @@ export function Player() {
                         src={episode.url}
                         ref={audioRef}
                         autoPlay
+                        onPlay={() => setPlayingState(true)}
+                        onPause={() => setPlayingState(false)}
                      />
                 ) }
 
