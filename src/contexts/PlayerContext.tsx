727e585aa9062ae480d34fce8793ce5cdfd1ced6
Erick Macedo Pinto <erimacedo_92@hotmail.com>
2021-04-27 01:20:29 -0300

    Adds a const to the exports of the PlayerContext

--- src/contexts/PlayerContext.tsx
+++ src/contexts/PlayerContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, ReactNode, useState } from "react";
+import { createContext, ReactNode, useContext, useState } from "react";
 
 type Episode = {
     title: string;
@@ -90,4 +90,8 @@ export function PlayerContextProvider({ children }: PlayerContextProviderProps) 
         </PlayerContext.Provider>
     );
             
+}
+
+export const usePlayer = () => {
+    return useContext(PlayerContext);
 }
